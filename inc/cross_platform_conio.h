affb8a256a552621f2c3eeec6f29cd905c1c3095
gor melkumyan <gormelqumyan6@gmail.com>
2025-08-27 16:39:51 +0400

    Windows

--- inc/cross_platform_conio.h
+++ inc/cross_platform_conio.h
@@ -6,6 +6,16 @@
 
 #include <cstdlib>
 
+void gotoxy(const int x, const int y);
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <conio.h>
+#elif defined(__unix__)
+    
+#else
+    #error "Unsupported platform. Cannot define SLEEP(n)."
+#endif
+
 #if defined(_WIN32) || defined(_WIN64)
     #include <windows.h>
     #define SLEEP(n) Sleep(n)
@@ -32,6 +42,7 @@
 #else
     #error "Unsupported platform. Cannot define values for keys."
 #endif  
+
 
 
 #endif // CROSS_PLATFORM_CONIO_H
--- src/cross_platform_conio.cpp
+++ src/cross_platform_conio.cpp
@@ -1,1 +1,9 @@
 #include "cross_platform_conio.h"
+
+void gotoxy(const int x, const int y) {
+
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
